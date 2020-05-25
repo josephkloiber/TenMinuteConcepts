4e96dedb69a401e069c4997a7bf5b07e43d2dadf
Joseph Kloiber <joseph.kloiber@gmail.com>
2020-05-25 10:31:38 -0400

    Added figure

--- experiment-sampling/piMonteCarlo.py
+++ experiment-sampling/piMonteCarlo.py
@@ -8,8 +8,8 @@ import matplotlib.pyplot as plt
 
 ################################################################################
 # set the geometrical bounds for the example
-samples = 1001
-radius = 1
+Samples = 1001
+Radius = 10
 
 save_image = 0
 
@@ -26,42 +26,42 @@ square_points = []
 circle_points = []
 ################################################################################
 
-def popluate_circle(radius):
+def popluate_circle(Radius):
     points = np.zeros([360,2])
     for step in range(0,360,1):
-        points[step] = [radius*np.cos(np.radians(step)), radius*np.sin(np.radians(step))]
+        points[step] = [Radius*np.cos(np.radians(step)), Radius*np.sin(np.radians(step))]
     return(points)
 
-def populate_square(radius):
+def populate_square(Radius):
     points = np.zeros([5, 2])
-    points[0] = [-radius, -radius]
-    points[1] = [radius, -radius]
-    points[2] = [radius, radius]
-    points[3] = [-radius, radius]
-    points[4] = [-radius, -radius]
+    points[0] = [-Radius, -Radius]
+    points[1] = [Radius, -Radius]
+    points[2] = [Radius, Radius]
+    points[3] = [-Radius, Radius]
+    points[4] = [-Radius, -Radius]
     return(points)
 
-def monte_carlo_points_2d(levels, radius):
+def monte_carlo_points_2d(levels, Radius):
     # sample count number of random points inside a square of dimensions 2R
-    return  np.random.rand(levels,2) * (2 * radius) - radius
+    return  np.random.rand(levels,2) * (2 * Radius) - Radius
 
-def latin_square_points_2d(levels, radius):
+def latin_square_points_2d(levels, Radius):
     # Compute the main diagonal of points
-    pitch = (2 * radius) / levels
+    pitch = (2 * Radius) / levels
     points = np.zeros([levels,2])
-    bgn = -radius + (0.5 * pitch)
+    bgn = -Radius + (0.5 * pitch)
     x_file = np.random.permutation(levels) # X's
     y_rank = np.random.permutation(levels) # Y's
     for level in range(levels):
          points[level] = [bgn+x_file[level]*pitch, bgn+y_rank[level]*pitch]
     return points
 
-def get_encircled_points(points, radius):
+def get_encircled_points(points, Radius):
     del circled_x[:]
     del circled_y[:]
     for i in range(np.size(points[:,0])):
         magnitude=np.sqrt(np.power(points[i,0], 2)+np.power(points[i,1], 2))
-        if(magnitude <= radius):
+        if(magnitude <= Radius):
             circled_x.append( points[i,0] )
             circled_y.append( points[i,1] )
     return
@@ -70,56 +70,91 @@ def count_encircled_points(points):
     magnitude=np.power(np.power(points[:,0], 2) + np.power(points[:,1], 2), 0.5)
     return np.size(np.where(magnitude <= 1))
 
-def plot_configuration(index, radius, title):
+def plot_configuration(index, Radius, title):
     plt.figure(index)
-    plt.xlim(-radius, radius)
-    plt.ylim(-radius, radius)
+    plt.xlim(-Radius, Radius)
+    plt.ylim(-Radius, Radius)
     plt.title(title)
     plt.xlabel("Width")
     plt.ylabel("Height")
     plt.axis('equal')
 
 ################################################################################
+# ---- Building the data ---
+#
+
 # 1. Display data 
 
 # 1a. The Square, Circle and Radius
-square_points = populate_square(radius)
-circle_points = popluate_circle(radius)
-radius_plot = np.zeros([2, 2])
-radius_plot[0] = ([0, 0])
-radius_plot[1] = ([radius, 0])
+square_points = populate_square(Radius)
+circle_points = popluate_circle(Radius)
+Radius_plot = np.array([[Radius, 0]])
 
 ## 1b. Pi Truth Value for plotting
 truth = np.zeros([2, 2])
 truth[0] = ([0, np.pi])
-truth[1] = ([samples, np.pi])
+truth[1] = ([Samples, np.pi])
 
 # 2. Build Monte Carlo Samples
-mc_points = monte_carlo_points_2d(samples, radius)
+mc_points = monte_carlo_points_2d(Samples, Radius)
 
 # 3. Build Latin Square Samples
-ls_points = latin_square_points_2d(samples, radius)
+ls_points = latin_square_points_2d(Samples, Radius)
 
 # 4. Calculate the Monte Carlo Variances
 mc_pi_estimates = []
-for count in range(1,samples):
+for count in range(1,Samples):
     mc_pi_estimates.append(4* count_encircled_points(mc_points[:count]) / count)
     mc_pi_est_var.append(np.var(mc_pi_estimates))
 
 # 5. Calculate the Latin Square Variances
 ls_pi_estimates = []
-for count in range(1,samples):
+for count in range(1,Samples):
     ls_pi_estimates.append(4* count_encircled_points(ls_points[:count]) / count)
     ls_pi_est_var.append(np.var(ls_pi_estimates))
 
 ################################################################################
 
+################################################################################
+# ---- Plotting the data ---
+#
+
+# 1. Plot the figures and Radius
+fig = plt.figure(1)
+plt.xlim(-Radius, Radius)
+plt.ylim(-Radius, Radius)
+plt.axis('equal')
+plt.axis('off')
+fig.set_facecolor("#dddddd")
+
+# ax = fig.add_subplot(1,1,1)
+# ax.text(5, 1, 'r', fontsize=15)
+plt.figtext(5,1,'r', fontsize=15)
+
+plt.plot(square_points[:,0], square_points[:,1], 'k--', linewidth=1)
+plt.plot(circle_points[:,0], circle_points[:,1], 'k--', linewidth=1)
+
+plt.arrow(0,0,Radius,0, width=0.2, head_width=.7, length_includes_head=True)
+
+plt.arrow(0,-Radius,Radius,0,width=0.2,head_width=.7,length_includes_head=True)
+plt.arrow(0,-Radius,-Radius,0,width=0.2,head_width=.7,length_includes_head=True)
+
+plt.arrow(-Radius,0,0,Radius,width=0.2,head_width=.7,length_includes_head=True)
+plt.arrow(-Radius,0,0,-Radius,width=0.2,head_width=.7,length_includes_head=True)
+
+#plt.plot(Radius_plot[:,0], Radius_plot[:,0], Radius_plot[:,1], 'v-', linewidth=5)
+plt.show()
+
+exit()
+
+
+################################################################################
 
     
 ################################################################################
 # Build and show the Monte Carlo points
 
-plot_configuration(1, radius, "1000 Random Samples Monte Carlo Sampling")
+plot_configuration(1, Radius, "1000 Random Samples Monte Carlo Sampling")
 
 plt.plot(mc_points[:,0], mc_points[:,1], 'bo', label='Monte Carlo')
 plt.plot(square_points[:,0], square_points[:,1], 'r-', linewidth=5)
@@ -127,7 +162,7 @@ plt.plot(circle_points[:,0], circle_points[:,1], 'g-', linewidth=5)
 if(save_image != 0):
     plt.savefig('./images/monte_carlo_points.png')
 
-get_encircled_points(mc_points, radius)
+get_encircled_points(mc_points, Radius)
 plt.plot(circled_x, circled_y, 'ko' )
 plt.plot(square_points[:,0], square_points[:,1], 'r-', linewidth=5)
 plt.plot(circle_points[:,0], circle_points[:,1], 'g-', linewidth=5)
@@ -136,11 +171,11 @@ plt.plot(circle_points[:,0], circle_points[:,1], 'g-', linewidth=5)
 ################################################################################
 # Build and show the latin square points
 
-plot_configuration(2, radius, "1000 Random Samples Latin Square Sampling")
+plot_configuration(2, Radius, "1000 Random Samples Latin Square Sampling")
 
 
 plt.plot(ls_points[:,0], ls_points[:,1], 'bo', label='Monte Carlo')
-get_encircled_points(ls_points, radius)
+get_encircled_points(ls_points, Radius)
 plt.plot(circled_x, circled_y, 'ko' )
 plt.plot(square_points[:,0], square_points[:,1], 'r-', linewidth=5)
 plt.plot(circle_points[:,0], circle_points[:,1], 'g-', linewidth=5)
